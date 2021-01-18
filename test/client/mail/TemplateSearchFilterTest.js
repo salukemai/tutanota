9867658ab7b720f91eb57701a742f941adb5b370
fim <fim@tutao.de>
2021-01-18 11:43:21 +0100

    test cases fix

--- test/client/mail/TemplateSearchFilterTest.js
+++ test/client/mail/TemplateSearchFilterTest.js
@@ -6,135 +6,113 @@ import type {EmailTemplate} from "../../../src/api/entities/tutanota/EmailTempla
 import {createEmailTemplate} from "../../../src/api/entities/tutanota/EmailTemplate"
 import {createEmailTemplateContent} from "../../../src/api/entities/tutanota/EmailTemplateContent"
 
-//TODO: fix tests
-//
-// o.spec("TemplateSearchFilter", function () {
-// 	o("finds in title", function () {
-// 		const emailTemplate: EmailTemplate = createEmailTemplate({
-// 			tag: "more_steps_needed",
-// 			title: "Please provide more steps",
-// 			contents: [
-// 				createEmailTemplateContent({languageCode: "en", text: "We will require more information"}),
-// 				createEmailTemplateContent({languageCode: "de", text: "Wir benötigen mehr Informationen, um"})
-// 			]
-// 		})
-// 		o(searchInContent("Please provide", [emailTemplate])).deepEquals([emailTemplate])
-// 	})
-// 	o("finds in one content", function () {
-// 		const emailTemplate: Array<EmailTemplate> = [
-// 			createEmailTemplate({
-// 				tag: "more_steps_needed",
-// 				title: "Please provide more steps",
-// 				contents: [
-// 					createEmailTemplateContent(
-// 						{languageCode: "en", text: "We will require more information"}
-// 					)
-// 				]
-// 			}),
-// 			createEmailTemplate({
-// 				tag: "account_access",
-// 				title: "Account cannot be accessed",
-// 				contents: [
-// 					createEmailTemplateContent({languageCode: "en", text: "Cannot access account"}),
-// 					createEmailTemplateContent({languageCode: "de", text: "Zugriff auf Konto nicht möglich"})
-// 				]
-// 			}),
-// 		]
-// 		o(searchInContent("access", emailTemplate)).deepEquals([emailTemplate[1]])
-// 	})
-// 	o("finds in both content and one title", function () { // 2nd template should be found first
-// 		const emailTemplate: Array<EmailTemplate> = [
-// 			createEmailTemplate({
-// 				tag: "account_access_lost",
-// 				title: "Lost access",
-// 				contents: [
-// 					createEmailTemplateContent({languageCode: "en", text: "Lost access to account"}),
-// 					createEmailTemplateContent({languageCode: "de", text: "Zugangsdaten zum Konto verloren"})
-// 				]
-// 			}),
-// 			createEmailTemplate({
-// 				tag: "account_refund",
-// 				title: "Account Refund",
-// 				contents: [
-// 					createEmailTemplateContent({languageCode: "en", text: "Refund account"}),
-// 					createEmailTemplateContent({languageCode: "de", text: "Zahlung stornieren"})
-// 				]
-// 			})
-// 		]
-// 		o(searchInContent("account", emailTemplate)).deepEquals([emailTemplate[1], emailTemplate[0]])
-// 	})
-// 	o("finds in tag", function () {
-// 		const emailTemplate: EmailTemplate = createEmailTemplate({
-// 			tag: "#account_info",
-// 			title: "Account information required",
-// 			contents: [
-// 				createEmailTemplateContent({languageCode: "en", text: "Please provide more information regarding"}),
-// 				createEmailTemplateContent({languageCode: "de", text: "Wir benötigen weitere Informationen, um"})
-// 			]
-// 		})
-// 		o(searchForTag("account_info", [emailTemplate])).deepEquals([emailTemplate])
-// 	})
-// 	o("finds in multiple tags", function () {
-// 		const emailTemplate: Array<EmailTemplate> = [
-// 			createEmailTemplate({
-// 				tag: "#account_access",
-// 				title: "Cannot access account",
-// 				contents: [
-// 					createEmailTemplateContent({languageCode: "en", text: "If you cannot access your account"}),
-// 					createEmailTemplateContent({languageCode: "de", text: "Wenn Sie auf Ihr Konto nicht zugreifen können"})
-// 				]
-// 			}),
-// 			createEmailTemplate({
-// 				tag: "#account_refund",
-// 				title: "Refund account",
-// 				contents: [
-// 					createEmailTemplateContent({languageCode: "en", text: "Refund account"}),
-// 					createEmailTemplateContent({languageCode: "de", text: "Zahlung stonieren"})
-// 				]
-// 			})
-// 		]
-// 		o(searchForTag("account", emailTemplate)).deepEquals(emailTemplate)
-// 	})
-// 		o("finds in content with multiple languages", function () {
-// 			const emailTemplate: Array<EmailTemplate> = [
-// 				createEmailTemplate({
-// 					tag: "account_refund",
-// 					title: "Access",
-// 					contents: [
-// 						createEmailTemplateContent({languageCode: "en", text: "Account access"}),
-// 						createEmailTemplateContent({languageCode: "de", text: "Account Zugriff"})
-// 					]
-// 				}),
-// 				createEmailTemplate({
-// 					tag: "account_refund",
-// 					title: "Refund",
-// 					contents: [
-// 						createEmailTemplateContent({languageCode: "en", text: "Account refund"}),
-// 						createEmailTemplateContent({languageCode: "de", text: "Account stornieren"})
-// 					]
-// 				})
-// 			]
-// 			o(searchInContent("Account", emailTemplate)).deepEquals(emailTemplate)
-// 		})
-// 		o("finds in content with multiple languages and in second title", function () {
-// 			const emailTemplate: Array<EmailTemplate> = [
-// 				createEmailTemplate({
-// 					tag: "account_refund",
-// 					title: "Access",
-// 					contents: [
-// 						createEmailTemplateContent({languageCode: "en", text: "Account refund"}),
-// 						createEmailTemplateContent({languageCode: "de", text: "Account stornieren"})
-// 					],
-// 				}),
-// 				createEmailTemplate({
-// 					tag: "null",
-// 					title: "Account refund",
-// 					contents: [
-// 						createEmailTemplateContent({languageCode: "en", text: "To refund a payment"}),
-// 						createEmailTemplateContent({languageCode: "de", text: "Zahlung stornieren"})
-// 					]
-// 				}),
-// 			]
-// 			o(searchInContent("account", emailTemplate)).deepEquals([emailTemplate[1], emailTemplate[0]]) // 2nd template should be found first
-// 		})
-// 	})
+
+o.spec("TemplateSearchFilter", function () {
+	o("finds in title", function () {
+		const emailTemplate: EmailTemplate = createEmailTemplate({
+			tag: "more_steps_needed",
+			title: "Please provide more steps",
+			contents: [
+				createEmailTemplateContent({languageCode: "en", text: "We will require more information"}),
+				createEmailTemplateContent({languageCode: "de", text: "Wir benötigen mehr Informationen, um"})
+			]
+		})
+		o(searchInTemplates("Please provide", [emailTemplate])).deepEquals([emailTemplate])
+	})
+	o("finds in one content", function () {
+		const emailTemplate: Array<EmailTemplate> = [
+			createEmailTemplate({
+				tag: "more_steps_needed",
+				title: "Please provide more steps",
+				contents: [
+					createEmailTemplateContent(
+						{languageCode: "en", text: "We will require more information"}
+					)
+				]
+			}),
+			createEmailTemplate({
+				tag: "account_access",
+				title: "Account cannot be accessed",
+				contents: [
+					createEmailTemplateContent({languageCode: "en", text: "Cannot access account"}),
+					createEmailTemplateContent({languageCode: "de", text: "Zugriff auf Konto nicht möglich"})
+				]
+			}),
+		]
+		o(searchInTemplates("access", emailTemplate)).deepEquals([emailTemplate[1]]) // should find the last one
+	})
+	o("finds in both content and one title", function () { // 2nd template should be found first
+		const emailTemplate: Array<EmailTemplate> = [
+			createEmailTemplate({
+				tag: "account_access_lost",
+				title: "Lost access",
+				contents: [
+					createEmailTemplateContent({languageCode: "en", text: "Lost access to account"}),
+					createEmailTemplateContent({languageCode: "de", text: "Zugangsdaten zum Konto verloren"})
+				]
+			}),
+			createEmailTemplate({
+				tag: "account_refund",
+				title: "Account Refund",
+				contents: [
+					createEmailTemplateContent({languageCode: "en", text: "Refund account"}),
+					createEmailTemplateContent({languageCode: "de", text: "Zahlung stornieren"})
+				]
+			})
+		]
+		o(searchInTemplates("account", emailTemplate)).deepEquals([emailTemplate[0], emailTemplate[1]])
+	})
+	o("finds in tag", function () {
+		const emailTemplate: EmailTemplate = createEmailTemplate({
+			tag: "#account_info",
+			title: "Account information required",
+			contents: [
+				createEmailTemplateContent({languageCode: "en", text: "Please provide more information regarding"}),
+				createEmailTemplateContent({languageCode: "de", text: "Wir benötigen weitere Informationen, um"})
+			]
+		})
+		o(searchInTemplates("account_info", [emailTemplate])).deepEquals([emailTemplate])
+	})
+	o("finds in multiple tags", function () {
+		const emailTemplate: Array<EmailTemplate> = [
+			createEmailTemplate({
+				tag: "#account_access",
+				title: "Cannot access account",
+				contents: [
+					createEmailTemplateContent({languageCode: "en", text: "If you cannot access your account"}),
+					createEmailTemplateContent({languageCode: "de", text: "Wenn Sie auf Ihr Konto nicht zugreifen können"})
+				]
+			}),
+			createEmailTemplate({
+				tag: "#account_refund",
+				title: "Refund account",
+				contents: [
+					createEmailTemplateContent({languageCode: "en", text: "Refund account"}),
+					createEmailTemplateContent({languageCode: "de", text: "Zahlung stonieren"})
+				]
+			})
+		]
+		o(searchInTemplates("account", emailTemplate)).deepEquals(emailTemplate)
+	})
+	o("finds in content with multiple languages", function () {
+		const emailTemplate: Array<EmailTemplate> = [
+			createEmailTemplate({
+				tag: "account_refund",
+				title: "Access",
+				contents: [
+					createEmailTemplateContent({languageCode: "en", text: "Account access"}),
+					createEmailTemplateContent({languageCode: "de", text: "Account Zugriff"})
+				]
+			}),
+			createEmailTemplate({
+				tag: "account_refund",
+				title: "Refund",
+				contents: [
+					createEmailTemplateContent({languageCode: "en", text: "Account refund"}),
+					createEmailTemplateContent({languageCode: "de", text: "Account stornieren"})
+				]
+			})
+		]
+		o(searchInTemplates("Account", emailTemplate)).deepEquals(emailTemplate)
+	})
+})

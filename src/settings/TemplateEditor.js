faeab5f1f689a618a59d48487772eafde5503548
fim <fim@tutao.de>
2021-01-27 15:23:58 +0100

    TemplateEditor & TemplateEditorModel refactoring

--- src/settings/TemplateEditor.js
+++ src/settings/TemplateEditor.js
@@ -3,7 +3,6 @@
 import m from "mithril"
 import {HtmlEditor} from "../gui/base/HtmlEditor"
 import stream from "mithril/stream/stream.js"
-import {assertNotNull, neverNull, noOp} from "../api/common/utils/Utils"
 import type {TextFieldAttrs} from "../gui/base/TextFieldN"
 import {TextFieldN} from "../gui/base/TextFieldN"
 import type {DialogHeaderBarAttrs} from "../gui/base/DialogHeaderBar"
@@ -13,10 +12,10 @@ import {Dialog} from "../gui/base/Dialog"
 import {Icons} from "../gui/base/icons/Icons"
 import {createDropdown} from "../gui/base/DropdownN"
 import {DropDownSelector} from "../gui/base/DropDownSelector"
-import {lang, languageByCode} from "../misc/LanguageViewModel"
+import {lang} from "../misc/LanguageViewModel"
 import type {LanguageCode} from "../misc/LanguageViewModel"
 import type {EmailTemplate} from "../api/entities/tutanota/EmailTemplate"
-import {TemplateEditorModel} from "./TemplateEditorModel"
+import {getLanguageCode, getLanguageName, TemplateEditorModel} from "./TemplateEditorModel"
 import {locator} from "../api/main/MainLocator"
 import type {TemplateGroupRoot} from "../api/entities/tutanota/TemplateGroupRoot"
 import {UserError} from "../api/common/error/UserError"
@@ -35,7 +34,6 @@ export function showTemplateEditor(template: ?EmailTemplate, templateGroupRoot: 
 	}
 
 	const saveAction = () => {
-		console.log("save")
 		editorModel.save()
 		           .then(() => {
 			           dialog.close()
@@ -76,23 +74,12 @@ class TemplateEditor implements MComponent<TemplateEditorAttrs> {
 			return this._templateContentEditor.getValue()
 		})
 
-		// init all values
-		const clientLanguageCode = lang.code
-		if (model.isUpdate()) { // if existing entry
-			// push to added languages
-			model.initAddedLanguages(model.template.contents)
-			// init selected Language
-			model.selectedLanguage(model.isClientLanguageInContent() ? clientLanguageCode : model.getAddedLanguages()[0].code)
-			// set editor values
-			model.title(model.template.title)
-			model.tag(model.template.tag || "")
-			const content = assertNotNull(model.template.contents.find(templateContent =>
-				templateContent.languageCode === model.selectedLanguage()))
-				.text
-			this._templateContentEditor.setValue(content)
-		} else { // if it's a new template set the default language
-			model.pushToAddedLanguages(languageByCode[clientLanguageCode])
-			model.selectedLanguage(clientLanguageCode)
+		// init all input fields
+		model.title(model.template.title)
+		model.tag(model.template.tag)
+		const content = model.selectedContent()
+		if (content) {
+			this._templateContentEditor.setValue(content.text)
 		}
 
 		// Initialize Attributes for TextFields and Buttons
@@ -108,7 +95,7 @@ class TemplateEditor implements MComponent<TemplateEditorAttrs> {
 
 		this._chooseLanguageAttrs = {
 			label: "language_label",
-			value: model.selectedLanguage.map((code) => model.getTranslatedLanguage(code)),
+			value: model.selectedContent.map((content) => content ? getLanguageName(content) : ""),
 			injectionsRight: () => [
 				model.getAddedLanguages().length > 1 ? m(ButtonN, removeButtonAttrs) : null,
 				m(ButtonN, languageButtonAttrs)
@@ -122,18 +109,14 @@ class TemplateEditor implements MComponent<TemplateEditorAttrs> {
 			icon: () => Icons.More,
 			click: createDropdown(() => {
 				// save current content with language & create a dropdwon with all added languages & an option to add a new language
-				if(model.isExisitingLanguage()) {
-					model.updateContent(this._templateContentEditor.getValue())
-				} else {
-					model.createContent(this._templateContentEditor.getValue())
-				}
-				let additionalLanguages = model.getReorganizedLanguages()
-				let buttons = model.getAddedLanguages().map(addedLanguage => {
+				model.updateContent()
+				let additionalLanguages = model.getAdditionalLanguages()
+				let buttons = model.template.contents.map(content => {
 					return {
-						label: () => lang.get(addedLanguage.textId),
+						label: () => getLanguageName(content),
 						click: () => {
-							model.selectedLanguage(addedLanguage.code)
-							this._templateContentEditor.setValue(model.getContentFromSelectedLanguage())
+							model.selectedContent(content)
+							this._templateContentEditor.setValue(content.text)
 						},
 						type: ButtonType.Dropdown
 					}
@@ -144,8 +127,10 @@ class TemplateEditor implements MComponent<TemplateEditorAttrs> {
 						let newLanguageCode: Stream<LanguageCode> = stream(additionalLanguages[0].value)
 						let dropDownSelector = new DropDownSelector("addLanguage_action", null, additionalLanguages, newLanguageCode, 250) // dropdown with all additional languages
 						let addLanguageOkAction = (dialog) => {
-							model.selectedLanguage(newLanguageCode())
-							model.pushToAddedLanguages(languageByCode[newLanguageCode()])
+							const newContent = model.createContent(newLanguageCode())
+							if (newContent) {
+								model.selectedContent(newContent)
+							}
 							this._templateContentEditor.setValue("")
 							dialog.close()
 						}
@@ -167,11 +152,11 @@ class TemplateEditor implements MComponent<TemplateEditorAttrs> {
 			icon: () => Icons.Trash,
 			Type: ButtonType.Action,
 			click: () => {
-				return Dialog.confirm(() => lang.get("deleteLanguageConfirmation_msg", {"{language}": model.getTranslatedLanguage(model.selectedLanguage())})).then((confirmed) => {
+				return Dialog.confirm(() => lang.get("deleteLanguageConfirmation_msg", {"{language}": (getLanguageName(model.selectedContent()))})).then((confirmed) => {
 					if (confirmed) {
 						model.removeContent()
-						model.selectedLanguage(model.getAddedLanguages()[0].code)
-						this._templateContentEditor.setValue(model.getContentFromSelectedLanguage())
+						model.selectedContent(model.template.contents[0])
+						this._templateContentEditor.setValue(model.selectedContent().text)
 					}
 					return confirmed
 				})
--- src/settings/TemplateEditorModel.js
+++ src/settings/TemplateEditorModel.js
@@ -1,45 +1,35 @@
 // @flow
 
 import type {Language, LanguageCode} from "../misc/LanguageViewModel"
-import {lang, languageByCode, languages} from "../misc/LanguageViewModel"
+import {getLanguage, lang, languageByCode, languages} from "../misc/LanguageViewModel"
 import type {EmailTemplateContent} from "../api/entities/tutanota/EmailTemplateContent"
 import type {EmailTemplate} from "../api/entities/tutanota/EmailTemplate"
 import {createEmailTemplateContent} from "../api/entities/tutanota/EmailTemplateContent"
-import {clone, downcast, noOp} from "../api/common/utils/Utils"
-import {TemplateModel} from "../templates/TemplateModel"
-import {locator} from "../api/main/MainLocator"
+import {clone, downcast} from "../api/common/utils/Utils"
 import {EntityClient} from "../api/common/EntityClient"
 import type {TemplateGroupRoot} from "../api/entities/tutanota/TemplateGroupRoot"
-import {createEmailTemplate} from "../api/entities/tutanota/EmailTemplate"
-import {NotFoundError} from "../api/common/error/RestError"
+import {createEmailTemplate, EmailTemplateTypeRef} from "../api/entities/tutanota/EmailTemplate"
 import stream from "mithril/stream/stream.js"
 import {UserError} from "../api/common/error/UserError"
-import {getElementId} from "../api/common/utils/EntityUtils"
-import {remove} from "../api/common/utils/ArrayUtils"
+import {contains, remove} from "../api/common/utils/ArrayUtils"
+import {getElementId, isSameId} from "../api/common/utils/EntityUtils"
+
 
 export class TemplateEditorModel {
-	_allLanguages: Array<Language>
-	_addedLanguages: Array<Language>
-	_templateModel: TemplateModel
-
+	template: EmailTemplate
 	title: Stream<string>
 	tag: Stream<string>
-	selectedLanguage: Stream<LanguageCode>
-	template: EmailTemplate
+	selectedContent: Stream<EmailTemplateContent>
 	_templateGroupRoot: TemplateGroupRoot
 	_entityClient: EntityClient
 	_contentProvider: ?() => string
 
-	constructor(template: ?EmailTemplate, templateGroupRoot: TemplateGroupRoot, entityClient: EntityClient) {
-		this._allLanguages = []
-		this._addedLanguages = []
-		this._templateModel = locator.templateModel
-		this.initAllLanguages()
 
+	constructor(template: ?EmailTemplate, templateGroupRoot: TemplateGroupRoot, entityClient: EntityClient) {
+		this.template = template ? clone(template) : createEmailTemplate()
 		this.title = stream("")
 		this.tag = stream("")
-		this.selectedLanguage = stream()
-		this.template = template ? clone(template) : createEmailTemplate()
+		this.selectedContent = stream(template ? template.contents[0] : this.createContent(lang.code))
 		this._templateGroupRoot = templateGroupRoot
 		this._entityClient = entityClient
 		this._contentProvider = null
@@ -49,159 +39,60 @@ export class TemplateEditorModel {
 		return this.template._id != null
 	}
 
-	initAllLanguages() {
-		languages.forEach(language => {
-			this._allLanguages.push(language)
-		})
-	}
-
-	initAddedLanguages(contents: EmailTemplateContent[]) {
-		for (const templateContents of contents) {
-			this._addedLanguages.push(languageByCode[getLanguageCode(templateContents)])
-		}
-	}
-
 	setContentProvider(provider: () => string) {
 		this._contentProvider = provider
 	}
 
-	createContent(editorValue: string): void {
-		const emailTemplateContent = createEmailTemplateContent({languageCode: this.selectedLanguage(), text: editorValue})
+	createContent(languageCode: LanguageCode): EmailTemplateContent {
+		const emailTemplateContent = createEmailTemplateContent({languageCode: languageCode, text: ""})
 		this.template.contents.push(emailTemplateContent)
+		return emailTemplateContent
 	}
 
-	updateContent(editorValue: string): void {
-		this.template.contents.forEach(content => {
-			if (content.languageCode === this.selectedLanguage()) {
-				content.text = editorValue
-			}
-		})
+	updateContent(): void {
+		const selectedContent = this.selectedContent()
+		if (selectedContent && this._contentProvider) {
+			selectedContent.text = this._contentProvider()
+		}
 	}
 
 	removeContent(): void {
-		const content = this._getExistingContent()
+		const content = this.selectedContent()
 		if (content) {
 			remove(this.template.contents, content)
-			remove(this._addedLanguages, languageByCode[this.selectedLanguage()])
 		}
 	}
 
-	isExisitingLanguage(): boolean {
-		return this.template.contents.some((content) => {
-			return content.languageCode === this.selectedLanguage()
-		})
-	}
-
-	_getUpdatedContent(editorValue: string): void {
-		this.template.contents.forEach(content => {
-			if (content.languageCode === this.selectedLanguage()) {
-				content.text = editorValue
-			}
-		})
-	}
-
-	_getExistingContent(): ?EmailTemplateContent {
-		for (const content of this.template.contents) {
-			if (content.languageCode === this.selectedLanguage()) {
-				return content
-			}
-		}
-		return null
-	}
-
-	pushToAddedLanguages(language: Language) {
-		this._addedLanguages.push(language)
-	}
-
-	getReorganizedLanguages(): Array<Object> { // sorts the languages, removes added languages from additional languages and then returns it
-		const sortedArray = this._allLanguages.map((l) => {
+	/**
+	 * Returns all languages that are available for creating new template content. Returns them in alphabetic order sorted by name.
+	 * @returns {Array<{name: string, value: LanguageCode}>}
+	 */
+	getAdditionalLanguages(): Array<{name: string, value: LanguageCode}> {
+		const translatedLanguages = languages.map((l) => {
 			return {name: lang.get(l.textId), value: l.code}
 		})
-		sortedArray.sort(function (a, b) { // Sort
-			var textA = a.name.toUpperCase();
-			var textB = b.name.toUpperCase();
-			return (textA < textB) ? -1 : (textA > textB) ? 1 : 0
-		})
-		let j
-		for (j = 0; j < this._addedLanguages.length; j++) {
-			let k
-			for (k = 0; k < sortedArray.length; k++) {
-				if (sortedArray[k].value === this._addedLanguages[j].code) {
-					sortedArray.splice(k, 1)
-				}
-			}
-		}
-		return sortedArray
-	}
+		translatedLanguages.sort((a, b) => a.name.localeCompare(b.name))
 
-
-	removeLanguageFromAddedLanguages(languageCode: LanguageCode) {
-		this._addedLanguages.splice(this._findIndex(languageCode), 1)
-	}
-
-	_findIndex(languageCode: LanguageCode): number { // temporary fix
-		let i
-		for (i = 0; i < this._addedLanguages.length; i++) {
-			if (this._addedLanguages[i].code === languageCode) {
-				return i
-			}
-		}
-		return -1
+		const addedLanguages = this.getAddedLanguages()
+		return translatedLanguages.filter(translatedLanguage => !contains(addedLanguages, languageByCode[downcast(translatedLanguage.value)]))
 	}
 
 	getAddedLanguages(): Array<Language> {
-		return this._addedLanguages
+		return this.template.contents.map(content => languageByCode[getLanguageCode(content)])
 	}
 
-	getContentFromSelectedLanguage(): string { // returns the value of the content as string
-		for (const content of this.template.contents) {
-			if (content.languageCode === this.selectedLanguage()) {
-				return content.text
-			}
+	tagAlreadyExists(): Promise<boolean> {
+		if (this.template._id) { // the current edited template should not be included in find()
+			return this._entityClient.loadAll(EmailTemplateTypeRef, this._templateGroupRoot.templates).then(allTemplates => {
+				const filteredTemplates = allTemplates.filter(template => !isSameId(getElementId(this.template), getElementId(template)))
+				return !!filteredTemplates.find(template => template.tag.toLowerCase() === this.template.tag.toLowerCase())
+			})
+		} else {
+			return this._entityClient.loadAll(EmailTemplateTypeRef, this._templateGroupRoot.templates).then(allTemplates => {
+				return !!allTemplates.find(template => template.tag.toLowerCase() === this.template.tag.toLowerCase())
+			})
 		}
-		return ""
 	}
-
-	getTranslatedLanguage(code: LanguageCode): string {
-		return lang.get(languageByCode[code].textId)
-	}
-
-	isClientLanguageInContent(): boolean { // checks if passed Language is in content of selected Template
-		const clientLanguageCode = lang.code
-		for (const templateContent of this.template.contents) {
-			if (templateContent.languageCode === clientLanguageCode) {
-				return true
-			}
-		}
-		return false
-	}
-
-	hasContent(template: EmailTemplate): ?LanguageCode { // returns the language code for the language that has no content
-		let content
-		let languageCode
-		let hasContent
-		for (const languageContent of template.contents) {
-			content = languageContent.text
-			languageCode = getLanguageCode(languageContent)
-			hasContent = !!content.replace(/(<([^>]+)>)/ig, "").length
-			if (!hasContent) {
-				return languageCode
-			}
-		}
-		return null
-	}
-
-	//
-	// tagAlreadyExists(tag: string, currentTemplate: EmailTemplate): boolean {
-	// 	let filteredTemplates
-	// 	if (currentTemplate._id) { // null if it's a new template
-	// 		filteredTemplates = this._templateModel.getAllTemplates().filter(template => getElementId(template)
-	// 			!== getElementId(currentTemplate)) // filter the current template because otherwise you can't edit it
-	// 	} else {
-	// 		filteredTemplates = this._templateModel.getAllTemplates()
-	// 	}
-	// 	return filteredTemplates.some(template => template.tag.toLowerCase() === tag.toLowerCase())
-	// }
 
 	save(): Promise<*> {
 		if (!this.title()) {
@@ -210,34 +101,28 @@ export class TemplateEditorModel {
 		if (!this.tag()) {
 			return Promise.reject(new UserError("emptyTag_msg"))
 		}
-		if (this._contentProvider) { // save content of current language
-			const content = this._contentProvider()
-			if (this.isExisitingLanguage()) {
-				this.updateContent(content)
-			} else {
-				this.createContent(content)
-			}
-		}
-		const langWithNoContent = this.hasContent(this.template)
-		if (langWithNoContent) {
-			return Promise.reject(new UserError(() => lang.get("languageContentEmpty_msg", {"{language}": this.getTranslatedLanguage(langWithNoContent)})))
-		}
-
-		// TODO: handle template tag exists
+		this.updateContent()
 
 		this.template.title = this.title().trim()
 		this.template.tag = this.tag().trim()
 
-		if (this.template._id) {
-			return this._entityClient.update(this.template)
-			           .catch(NotFoundError, noOp)
-		} else {
-			this.template._ownerGroup = this._templateGroupRoot._id
-			return this._entityClient.setup(this._templateGroupRoot.templates, this.template)
-		}
+		return this.tagAlreadyExists().then(exists => {
+			if (exists) {
+				return Promise.reject(new UserError("templateTagExists_msg"))
+			} else if (this.template._id) {
+				return this._entityClient.update(this.template)
+			} else {
+				this.template._ownerGroup = this._templateGroupRoot._id
+				return this._entityClient.setup(this._templateGroupRoot.templates, this.template)
+			}
+		})
 	}
 }
 
 export function getLanguageCode(content: EmailTemplateContent): LanguageCode {
 	return downcast(content.languageCode)
+}
+
+export function getLanguageName(content: EmailTemplateContent): string {
+	return lang.get(languageByCode[getLanguageCode(content)].textId)
 }
